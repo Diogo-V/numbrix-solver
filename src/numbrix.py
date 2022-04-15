b588017fd954d33b5abeecf84d9f9e0d69ec56c8
Diogo-V <diogo.venancio00@gmail.com>
2022-04-15 20:06:44 +0100

    Added a heuristic function (I think, still don't understand this crap)
    Took 4 hours 27 minutes

--- src/numbrix.py
+++ src/numbrix.py
@@ -5,32 +5,11 @@
 # Grupo 06:
 # 95555 Diogo Venâncio
 # 95675 Sofia Morgado
-
+import copy
 import sys
 from typing import Tuple
 
 from search import Problem, Node, astar_search, breadth_first_tree_search, depth_first_tree_search, greedy_search, recursive_best_first_search
-
-
-class Action:
-    """Encapsulates action representation."""
-
-    def __init__(self, row, column, number):
-        self.row = row
-        self.column = column
-        self.number = number
-
-    def get_row(self):
-        return self.row
-
-    def get_column(self):
-        return self.column
-
-    def get_number(self):
-        return self.number
-
-    def to_tuple(self):
-        return self.row, self.column, self.number
 
 
 class NumbrixState:
@@ -52,6 +31,11 @@ class Board:
         self.n = len(init_matrix)
         self.max_value = self.n * self.n
         self.matrix = init_matrix
+        self.available = self.get_available_board_values()
+
+    def get_board(self) -> list[list[int]]:
+        """Returns a matrix representation"""
+        return self.matrix
     
     def get_number(self, row: int, col: int) -> int:
         """Returns number in requested position."""
@@ -64,27 +48,27 @@ class Board:
     def adjacent_vertical_numbers(self, row: int, col: int) -> tuple:
         """Returns values in upper and lower positions, respectively."""
         result = ()
-        try:
-            result += (self.matrix[row + 1][col], )
-        except IndexError:
+        if row == 0:
             result += (None, )
-        try:
+        else:
             result += (self.matrix[row - 1][col], )
-        except IndexError:
+        if row == self.n - 1:
             result += (None, )
+        else:
+            result += (self.matrix[row + 1][col],)
         return result
 
     def adjacent_horizontal_numbers(self, row: int, col: int) -> tuple:
         """ Returns values to the left and to right, respectively."""
         result = ()
-        try:
+        if col == 0:
+            result += (None, )
+        else:
             result += (self.matrix[row][col - 1], )
-        except IndexError:
+        if col == self.n - 1:
             result += (None, )
-        try:
+        else:
             result += (self.matrix[row][col + 1], )
-        except IndexError:
-            result += (None, )
         return result
 
     def get_available_board_values(self) -> list[int]:
@@ -113,42 +97,59 @@ class Numbrix(Problem):
         """Specifies initial state of the board."""
         super().__init__(initial=NumbrixState(board))
         self.board = board
-        self.available = self.board.get_available_board_values()
 
-    def actions(self, state: NumbrixState) -> list[Action]:
+    def get_position_valid_values(self, state: NumbrixState, row: int, col: int) -> list[int]:
+        """Returns a list of integers which represent the values that can be put in the input coordinates."""
+
+        result = []
+
+        # Gets restriction values adjacent to the currently being evaluated position
+        up, down = state.board.adjacent_vertical_numbers(row, col)
+        left, right = state.board.adjacent_horizontal_numbers(row, col)
+
+        # Goes over all the possible values for this board and appends the value if it satisfies the
+        # restrictions
+        for val in state.board.get_available_board_values():
+            if val != up and val != down and val != left and val != right:
+                result.append(val)
+
+        return result
+
+    def actions(self, state: NumbrixState) -> list[tuple[int, int, int]]:
         """Returns a list of actions that can be done on the input state."""
 
         result = []
 
         # Goes over each element in the
-        for i in range(self.board.n):
-            for j in range(self.board.n):
+        for i in range(state.board.n):
+            for j in range(state.board.n):
 
                 # Checks if this position is not already filled
-                if self.board.get_number(i, j) == 0:
+                if state.board.get_number(i, j) == 0:
 
-                    # Gets restriction values adjacent to the currently being evaluated position
-                    up, down = self.board.adjacent_vertical_numbers(i, j)
-                    left, right = self.board.adjacent_horizontal_numbers(i, j)
+                    # Get values that can be put in this position
+                    values = self.get_position_valid_values(state, i, j)
 
-                    # Goes over all the possible values for this board and appends the value if it satisfies the
-                    # restrictions
-                    for val in self.available:
-                        if val != up and val != down and val != left and val != right:
-                            result.append(Action(i, j, val))
+                    # Appends possible actions for this position
+                    for val in values:
+                        result.append((i, j, val))
 
         return result
 
-    def result(self, state: NumbrixState, action: Action) -> NumbrixState:
+    def result(self, state: NumbrixState, action: tuple[int, int, int]) -> NumbrixState:
         """Returns the resulting state of applying the input action (result from self.action(state)) to
         the current state."""
 
         # Removes value from list of available values for this board
-        self.available.remove(action[2])
+        # self.available.remove(action[2])  TODO: decide what to do with this
 
         # Changes board value to the requested action and returns it
-        self.board.set_number(*action.to_tuple())
-        return NumbrixState(self.board)
+        # new_board = copy.deepcopy(self.board)  TODO: make sure it is not this
+        # new_board.set_number(*action)
+        new_state = copy.deepcopy(state)
+        new_state.board.set_number(*action)
+        new_state.board.available.remove(action[2])
+        return NumbrixState(new_state.board)
 
     def goal_test(self, state: NumbrixState) -> bool:
         """Checks if we have a valid solution of this game-"""
@@ -157,27 +158,27 @@ class Numbrix(Problem):
         row, col = 0, 0
 
         # Finds row and column where the solution starts (finds value=1)
-        for i in range(self.board.n):
-            for j in range(self.board.n):
-                if self.board.get_number(i, j) == 1:
+        for i in range(state.board.n):
+            for j in range(state.board.n):
+                if state.board.get_number(i, j) == 1:
                     row, col = i, j
 
         # Tries to go through the solution's path and check whether it is valid or not
         val = 1
-        while val < self.board.max_value:
+        while val < state.board.max_value:
 
             # Gets adjacent values to discover where is the next value located
-            up, down = self.board.adjacent_vertical_numbers(row, col)
-            left, right = self.board.adjacent_horizontal_numbers(row, col)
+            up, down = state.board.adjacent_vertical_numbers(row, col)
+            left, right = state.board.adjacent_horizontal_numbers(row, col)
 
             # Tries to see where is the next position
-            if up == self.board.get_number(row, col) + 1:
+            if up == state.board.get_number(row, col) + 1:
                 row -= 1
-            elif down == self.board.get_number(row, col) + 1:
+            elif down == state.board.get_number(row, col) + 1:
                 row += 1
-            elif left == self.board.get_number(row, col) + 1:
+            elif left == state.board.get_number(row, col) + 1:
                 col -= 1
-            elif right == self.board.get_number(row, col) + 1:
+            elif right == state.board.get_number(row, col) + 1:
                 col += 1
             else:
                 return False  # In this case, we were not able to find a suitable path and so, this is not a solution
@@ -188,8 +189,71 @@ class Numbrix(Problem):
 
     def h(self, node: Node):
         """Heuristic function used in A*"""
-        # TODO
-        pass
+        # TODO: ideas
+        #       -> Ver se o valor que está ao meu lado é +/- 1 que eu e dar mais pontos
+        #       -> Dar mais pontos à medida que vai formando um caminho
+        #       -> Somar linhas/colunas ??? (ideia do stor)
+
+        # All nodes get a +1 value from their heuristic function
+        total = 1
+
+        # We initialize our loop condition a true to be able to find a path
+        found_path = True
+
+        # This value is going to be used to "reward" a good path. Each time we find a path, this value is doubled. This
+        # allows us to be able to "reward" longer paths
+        total_to_be_added = 2
+
+        # Holds list of visited values (used to not repeat already visited positions)
+        visited = []
+
+        # If this node does not have an action applied to it, then we don't need to find a path
+        if node.action is None:
+            return total
+
+        # Gets action that is going to be taken on this node and unpacks it to get the adjacent nodes
+        row, col, val = node.action
+
+        while found_path:
+
+            # Gets adjacent values
+            up, down = node.state.board.adjacent_vertical_numbers(row, col)
+            left, right = node.state.board.adjacent_horizontal_numbers(row, col)
+
+            # Now, we try to form a path from this node by evaluating all adjacent nodes and see if they form a path too
+            # and if so, we update the current value and double the next score if we can find even more nodes that form
+            # a path
+            if up is not None and up != 0 and up not in visited:
+                if val == up + 1 or val == up - 1:
+                    total += total_to_be_added
+                    total_to_be_added *= 2
+                    row -= 1
+                    continue
+
+            if down is not None and down != 0 and down not in visited:
+                if val == down + 1 or val == down - 1:
+                    total += total_to_be_added
+                    total_to_be_added *= 2
+                    row += 1
+                    continue
+
+            if left is not None and left != 0 and left not in visited:
+                if val == left + 1 or val == left - 1:
+                    total += total_to_be_added
+                    total_to_be_added *= 2
+                    col -= 1continue
+
+            if right is not None and right != 0 and right not in visited:
+                if val == right + 1 or val == right - 1:
+                    total += total_to_be_added
+                    total_to_be_added *= 2
+                    col += 1
+                    continue
+
+            # In case no valid path is found, then we stop this loop and return the total accumulated heuristic value
+            found_path = False
+
+        return total
     
 
 if __name__ == "__main__":
@@ -203,7 +267,7 @@ if __name__ == "__main__":
         # Initializes Numbrix problem
         numbrix = Numbrix(board)
 
-        print("A*: \n", astar_search(numbrix))
+        print("A*: \n", astar_search(numbrix, display=True))
 
         # Prints solution in the stdout
         print(board.get_result())
