74a99c437a7034966a61899dbe5e17a847d5f738
Diogo-V <diogo.venancio00@gmail.com>
2022-04-23 17:56:54 +0100

    Fixed actions and result
    Took 6 hours 39 minutes

--- src/numbrix.py
+++ src/numbrix.py
@@ -7,6 +7,7 @@
 # 95675 Sofia Morgado
 import copy
 import sys
+import heapq as PQ
 
 from search import Problem, Node, astar_search, breadth_first_tree_search, depth_first_tree_search, greedy_search, recursive_best_first_search
 
@@ -30,40 +31,46 @@ class Board:
         self.n = len(init_matrix)
         self.max_value = self.n * self.n
         self.matrix = init_matrix
+        self.queue = []
+        PQ.heapify(self.queue)
         self.inserted, self.frontier = self.build_matrix_structs()
+        self.previous_action = None
 
     def build_matrix_structs(self):
         """Builds a dictionary with the values already in the board + their coordinates and also setups the frontier."""
 
-        def try_check(row, col, matrix, matrix_col_size):
-            if 0 <= row < matrix_col_size and 0 <= col < matrix_col_size:
-                if matrix[row][col] == 0:
+        def build_frontier(row, col, board):
+            if 0 <= row < board.n and 0 <= col < board.n:
+                if board.matrix[row][col] == 0:
                     if (row, col) not in frontier:
-                        possible_values = Board.is_frontier(self, row, col, inserted)
+                        possible_values = Board.get_possible_values(self, row, col, inserted)
                         frontier[(row, col)] = possible_values
-
-        inserted = {}
-        frontier = {}
+                        PQ.heappush(board.queue, (len(possible_values), (row, col)))
 
         # Goes over all positions in the matrix and checks if they are already filled and stores their coordinates
+        inserted = {}
         for i in range(self.n):
             for j in range(self.n):
                 val = self.get_number(i, j)
                 if val != 0:
-
                     inserted[val] = (i, j)
 
-                    # Stores possible values for the adjacent nodes
-                    try_check(i + 1, j, self.matrix, self.n)
-                    try_check(i - 1, j, self.matrix, self.n)
-                    try_check(i, j + 1, self.matrix, self.n)
-                    try_check(i, j - 1, self.matrix, self.n)
+        # Builds frontiers after knowing which nodes have been inserted
+        frontier = {}
+        for (i, j) in inserted.values():
+            build_frontier(i + 1, j, self)
+            build_frontier(i - 1, j, self)
+            build_frontier(i, j + 1, self)
+            build_frontier(i, j - 1, self)
+
+        print("Inserted: ", inserted)
+        print("Frontier: ", frontier)
 
         return inserted, frontier
 
     @staticmethod
-    def is_frontier(board, row, col, inserted):
-        """Checks if coordinate is frontier and if so, stores its possible values and returns them."""
+    def get_possible_values(board, row, col, inserted):
+        """Returns possible values for input coordinate. Returns empty list if none are found."""
 
         def check(border):
             result1 = []
@@ -87,6 +94,34 @@ class Board:
         result.extend(check(right))
 
         return result
+
+    @staticmethod
+    def update_adjacent_frontier(board, row, col, value):
+        """Updates board frontier values when 'value' is inserted in input coordinates."""
+
+        # Gets currently open adjacent coordinates (these are the only ones that can get updated)
+        up, down = board.adjacent_vertical_numbers(row, col)
+        left, right = board.adjacent_horizontal_numbers(row, col)
+
+        if up == 0:
+            board.remove_frontier(board, row - 1, col, value)
+            # TODO: update queue
+        if down == 0:
+            board.remove_frontier(board, row + 1, col, value)
+        if left == 0:
+            board.remove_frontier(board, row, col - 1, value)
+        if right == 0:
+            board.remove_frontier(board, row, col + 1, value)
+
+    @staticmethod
+    def remove_frontier(board, row, col, value):
+        """Removes value from frontier input coordinate."""
+        try:
+            print("Removing from frontier: ", (row, col, value))
+            print("Frontier: ", board.frontier)
+            board.frontier[(row, col)].remove(value)
+        except ValueError:
+            pass
 
     def get_board(self):
         """Returns a matrix representation"""
@@ -152,66 +187,89 @@ class Numbrix(Problem):
     def actions(self, state):
         """Returns a list of actions that can be done on the input state."""
 
-        # TODO:
-        #   -> Implement frontier nodes
-        #   -> Implement Priority Queue
+        # TODO (ideias):
+        #   -> Implement frontier nodes (done):
+        #       -> Sempre que é  alterada uma coordenada da fronteira, as únicas fronteiras que mudam são as dos
+        #          valores [val + 1, val - 1]
+        #   -> Implement Priority Queue (done):
+        #       -> Vai usar uma tupla -len, (coordinates)) para obter a posição com menos valores possiveis
+        #       -> Vai ter referencia para os valores com menos posições possiveis?
         #   -> Implement Aglomerados e os pontos atribuidos
         #   -> Implement nós lança (nós nas pontas dos aglomerados das listas)
 
-        # Holds list of actions that can be taken
-        result = []
+        # If it is the first iteration, we don't need to do anything because it has already been done
+        if state.board.previous_action is None:
+            return [(row, col, val) for (row, col), values in state.board.frontier.items() for val in values]
 
-        # Holds the frontier for the next iteration
-        frontier = {}
+        # Unpacks previous action that was taken previously
+        row, col, result = state.board.previous_action
 
-        # Iterates over all the inserted items and returns the possible action for each one of them
-        for key, (row, col) in state.board.frontier.items():  # TODO: fix this
+        # Since we only need to update the values above and below (frontier property, we check if they are in the
+        # board and update their adjacent frontier values
+        if result + 1 in state.board.inserted:
+            adj_row, adj_col = state.board.inserted[result + 1]
+            Board.update_adjacent_frontier(state.board, adj_row, adj_col, result)
+        if result - 1 in state.board.inserted:
+            adj_row, adj_col = state.board.inserted[result - 1]
+            Board.update_adjacent_frontier(state.board, adj_row, adj_col, result)
 
-            # Gets restriction values adjacent to the currently being evaluated position
-            up, down = state.board.adjacent_vertical_numbers(row, col)
-            left, right = state.board.adjacent_horizontal_numbers(row, col)
+        # We also need to update our own adjacent frontier values
+        up, down = state.board.adjacent_vertical_numbers(row, col)
+        left, right = state.board.adjacent_horizontal_numbers(row, col)
+        if up == 0:  # TODO: update queue
+            state.board.frontier[(row - 1, col)] = Board.get_possible_values(state.board, row - 1, col, state.board.inserted)
+        if down == 0:
+            state.board.frontier[(row + 1, col)] = Board.get_possible_values(state.board, row + 1, col, state.board.inserted)
+        if left == 0:
+            state.board.frontier[(row, col - 1)] = Board.get_possible_values(state.board, row, col - 1, state.board.inserted)
+        if right == 0:
+            state.board.frontier[(row, col + 1)] = Board.get_possible_values(state.board, row, col + 1, state.board.inserted)
 
-            # Gets available adjacent values
-            available = [i for i in [key + 1, key - 1] if i not in state.board.inserted and 0 < i < state.board.max_value]
-
-            # Iterates over the possible adjacent values and appends possible actions to the result list
-            for val in available:
-
-                # We need to check if our adjacency is an empty position before appending the action. Also adds this
-                # values to the not yet visited state
-                if up == 0:
-                    action = (row - 1, col, val)
-                    result.append(action)
-                    frontier[val] = (row - 1, col)
-                if down == 0:
-                    action = (row + 1, col, val)
-                    result.append(action)
-                    frontier[val] = (row + 1, col)
-                if left == 0:
-                    action = (row, col - 1, val)
-                    result.append(action)
-                    frontier[val] = (row, col - 1)
-                if right == 0:
-                    action = (row, col + 1, val)
-                    result.append(action)
-                    frontier[val] = (row, col + 1)
-
-        # Sets the not visited structure with the newly appended values since they have not yet been seen
-        state.board.not_visited = frontier
-
-        return result
+        # TODO: improve this by storing the previous array and only add/remove the changes that were made to it
+        return [(row, col, val) for (row, col), values in state.board.frontier.items() for val in values]
 
     def result(self, state, action):
         """Returns the resulting state of applying the input action (result from self.action(state)) to
         the current state."""
         new_state = copy.deepcopy(state)
         new_state.board.set_number(*action)
-        state.board.inserted[action[2]] = (action[0], action[1])
+        new_state.board.inserted[action[2]] = (action[0], action[1])
+        new_state.board.frontier.pop((action[0], action[1]))
+        new_state.board.previous_action = action
         return NumbrixState(new_state.board)
 
     def goal_test(self, state):
         """Checks if we have a valid solution of this game-"""
-        return len(state.board.inserted) == state.board.max_value
+
+        if len(state.board.inserted) != state.board.max_value:
+            return False
+
+        # Stores the start of this board (number one)
+        row, col = state.board.inserted[1]
+
+        # Tries to go through the solution's path and check whether it is valid or not
+        val = 1
+        while val < state.board.max_value:
+
+            # Gets adjacent values to discover where is the next value located
+            up, down = state.board.adjacent_vertical_numbers(row, col)
+            left, right = state.board.adjacent_horizontal_numbers(row, col)
+
+            # Tries to see where is the next position
+            if up == state.board.get_number(row, col) + 1:
+                row -= 1
+            elif down == state.board.get_number(row, col) + 1:
+                row += 1
+            elif left == state.board.get_number(row, col) + 1:
+                col -= 1
+            elif right == state.board.get_number(row, col) + 1:
+                col += 1
+            else:
+                return False  # In this case, we were not able to find a suitable path and so, this is not a solution
+
+            val += 1
+
+        return True
 
     def h(self, node):
         """Heuristic function used in A*"""
@@ -231,136 +289,10 @@ class Numbrix(Problem):
         #      é alterado (vou ter de ter dois dict em que coloco os nós ainda não vistos aquando do result())
         #   -> Fechar os nós que já não têm mais opções (aquando do result(), posso ter uma estrutura para isso?)
         #   -> Contar o nr de casas vazias por linha e somar ao valor que a heuristica dá
+        #   -> Ter uma matriz com todos os valores possiveis para cada possição. se algum array ficar vazio, dou um
+        #      um péssimo valor na heuristica porque quer dizer que o tabuleiro n tem solução
 
-        def check_opposite_side(val1, up1, down1, left1, right1, already_checked):
-            if up1 is not None and up1 != 0 and up1 not in visited and up1 != already_checked:
-                if val1 == up1 + 1 or val1 == up1 - 1:
-                    return up1, row - 1, col
-
-            if down1 is not None and down1 != 0 and down1 not in visited and down1 != already_checked:
-                if val1 == down1 + 1 or val1 == down1 - 1:
-                    return down1, row + 1, col
-
-            if left1 is not None and left1 != 0 and left1 not in visited and left1 != already_checked:
-                if val1 == left1 + 1 or val1 == left1 - 1:
-                    return left1, row, col - 1
-
-            if right1 is not None and right1 != 0 and right1 not in visited and right1 != already_checked:
-                if val1 == right1 + 1 or val1 == right1 - 1:
-                    return right1, row, col + 1
-
-            return ()
-
-        # All nodes are initialized with a very big number. This is done because we want to prioritize lower heuristic
-        # values over bigger ones
-        total = 1000000000
-
-        # We initialize our loop condition a true to be able to find a path
-        found_path = True
-
-        # This value is going to be used to "reward" a good path. Each time we find a path, this value is used to
-        # subtract from the total amount. This allows us to be able to "reward" longer paths
-        reward = 2
-
-        # Holds list of visited values (used to not repeat already visited positions)
-        visited = []
-
-        # If this node does not have an action applied to it, then we don't need to find a path
-        if node.action is None:
-            return total
-
-        # Gets action that is going to be taken on this node and unpacks it to get the adjacent nodes
-        row, col, val = node.action
-
-        # Holds iteration value
-        current_val = val
-
-        # Checks if we are seeing a value in the middle of a path and if so, we need to store the other side's info and
-        # proceed to the opposite one
-        has_opposite = False
-        iteration = 0
-        opp_val = -1
-        opp_row = -1
-        opp_col = -1
-
-        while found_path:
-
-            # Gets adjacent values
-            up, down = node.state.board.adjacent_vertical_numbers(row, col)
-            left, right = node.state.board.adjacent_horizontal_numbers(row, col)
-
-            # Now, we try to form a path from this node by evaluating all adjacent nodes and see if they form a path too
-            # and if so, we update the current value and double the next score if we can find even more nodes that form
-            # a path
-            if up is not None and up != 0 and up not in visited:
-                if current_val == up + 1 or current_val == up - 1:
-                    opposite = check_opposite_side(current_val, up, down, left, right, up) if iteration == 0 else ()
-                    iteration += 1
-                    if opposite != ():
-                        has_opposite = True
-                        opp_val, opp_row, opp_col = opposite
-                    total -= reward
-                    reward *= 2
-                    row -= 1
-                    visited.append(current_val)
-                    current_val = up
-                    continue
-
-            if down is not None and down != 0 and down not in visited:
-                if current_val == down + 1 or current_val == down - 1:
-                    opposite = check_opposite_side(current_val, up, down, left, right, down) if iteration == 0 else ()
-                    iteration += 1
-                    if opposite != ():
-                        has_opposite = True
-                        opp_val, opp_row, opp_col = opposite
-                    total -= reward
-                    reward *= 2
-                    row += 1
-                    visited.append(current_val)
-                    current_val = down
-                    continue
-
-            if left is not None and left != 0 and left not in visited:
-                if current_val == left + 1 or current_val == left - 1:
-                    opposite = check_opposite_side(current_val, up, down, left, right, left) if iteration == 0 else ()
-                    iteration += 1
-                    if opposite != ():
-                        has_opposite = True
-                        opp_val, opp_row, opp_col = opposite
-                    total -= reward
-                    reward *= 2
-                    col -= 1
-                    visited.append(current_val)
-                    current_val = left
-                    continue
-
-            if right is not None and right != 0 and right not in visited:
-                if current_val == right + 1 or current_val == right - 1:
-                    opposite = check_opposite_side(current_val, up, down, left, right, right) if iteration == 0 else ()
-                    iteration += 1
-                    if opposite != ():
-                        has_opposite = True
-                        opp_val, opp_row, opp_col = opposite
-                    total -= reward
-                    reward *= 2
-                    col += 1
-                    visited.append(current_val)
-                    current_val = right
-                    continue
-
-            if has_opposite:  # Checks if we had more to the other side
-                current_val = opp_val
-                row = opp_row
-                col = opp_col
-                total -= reward
-                reward *= 2
-                has_opposite = False
-                continue
-
-            # In case no valid path is found, then we stop this loop and return the total accumulated heuristic value
-            found_path = False
-
-        return total
+        return 1
 
 
 if __name__ == "__main__":
@@ -372,8 +304,7 @@ if __name__ == "__main__":
     numbrix = Numbrix(board)
 
     # Applies our search algorithm to find the correct solution
-    # result = astar_search(numbrix).state.board.get_result()
-    result = depth_first_tree_search(numbrix).state.board.get_result()
+    result = astar_search(numbrix).state.board.get_result()
 
     # Shows result in stdin
     print(result, end="")
